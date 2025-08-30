973abca2b7d347b105d53f6049150c2a96383b5f
light2a <86029943+light2a@users.noreply.github.com>
2025-08-30 21:44:56 +0700

    chore: initial template setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
